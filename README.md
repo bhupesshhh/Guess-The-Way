af7061c3dc0d9d8b108f6d8ede04d78a5d76f30b
bhupesshhh <bhupeshchandrawanshi@gmail.com>
2021-03-19 07:23:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Welcome to this word game called - "GUESS THE WAY".
+This is a simple word game based on python rules and condtions.
+
+Game Syntax:
+
+1. Player Setup (Name and Age)
+2. Game Setup (Start the game)
+3. Stage Setup (Random Stages)
+4. Points Upgrae (Based on stage clearance)
+5. Game End (End of game, Score and round tally)
+
+I used import, function, input, print, while and nested loops to create a simple puzzle game.
+
+imports = Random Module for random stage setup and random answer
+input = for name,age, answers for each stage
+functions = for stages (hill,jungle,lake,river)
+loops = Nested loop for gameplay
---
+++ main.py
@@ -0,0 +1,125 @@
+import random
+
+
+# Set The Initial Game Values
+points = 0
+round = 0
+game_on = False
+
+# Stage Jungle
+def jungle():
+  global game_on
+  print("Welcome to Jungle")
+  jun = ["pass","jump"]
+  ans = input("Would you pass through or jump around the jungle? (pass/jump) ")
+  jungle_answer = random.choice(jun)
+
+  # Check for the answer
+  if ans.lower() == jungle_answer:
+    game_on = True
+    print("Right guess, your choice is correct!")
+  else:
+    game_on = False
+    print("Wrong guess, your choice is incorrect!")
+
+# Stage Hill
+def hill():
+  global game_on
+  print("Welcome to Hill")
+  hil = ["pass","jump"]
+  ans = input("Would you pass through or jump around the hill? (pass/jump) ")
+  hill_answer = random.choice(hil)
+
+  # Check for the answer
+  if ans.lower() == hill_answer:
+    game_on = True
+    print("Right guess, your choice is correct!")
+  else:
+    game_on = False
+    print("Wrong guess, your choice is incorrect!")
+
+# Stage River
+def river():
+  global game_on
+  print("Welcome to River")
+  riv = ["swim","ride"]
+  ans = input("Would you swim through or ride across the river? (swim/ride) ")
+  river_answer = random.choice(riv)
+
+  # Check for the answer
+  if ans.lower() == river_answer:
+    game_on = True
+    print("Right guess, your choice is correct!")
+  else:
+    game_on = False
+    print("Wrong guess, your choice is incorrect!")
+
+# Stage Lake 
+def lake():
+  global game_on
+  print("Welcome to lake")
+  lak = ["swim","pass"]
+  ans = input("Would you pass through or swim across the lake? (pass/swim) ")
+  lake_answer = random.choice(lak)
+
+  #Check for the answer
+  if ans.lower() == lake_answer:
+    game_on = True
+    print("Right guess, your choice is correct!") 
+  else:
+    game_on = False
+    print("Wrong guess, your choice is incorrect!")
+
+# Gameplay 
+print("Welcome to this word game called..\n")
+print("GUESS THE WAY!!!\n")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+print(f"\nHello {name}, you are {age} years old.")
+
+# Start the Game
+if age > 10:
+  print("You are eligible to play the game.\n")
+  gameplay = input("Do you wanna play the game? (yes/no) ")
+
+  # Check for the answer
+  if gameplay.lower() == 'yes':
+    print("Let's start the game.\n")
+    game_on = True
+  else:
+    print("We hope to see you soon.") 
+else:
+	print("You are not eligible to play the game.")
+
+
+# Game Initiation Measures
+stage = [lake,jungle,river,hill]
+
+# Gameplay
+while game_on:
+  print(f"Round: {round}")
+
+  stage_play = random.choice(stage)
+  stage_play()
+
+  if game_on == False:
+    break
+  else:
+    points += 10
+
+  print(f"Score:{points}\n")
+  round += 1
+
+  if round == 20:
+    game_on = False
+    print("You won the game!")
+    break
+
+# Final Gameplay Scores
+print("\n")
+print(f"Thanks for joining {name}.")
+if round < 20:
+  print(f"You comepletd {round} rounds.")
+else:
+  print("You comepleted all rounds.")
+print(f"Your final score is {points}")
